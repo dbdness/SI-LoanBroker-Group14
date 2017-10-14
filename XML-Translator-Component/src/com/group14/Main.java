bad860659b8557212dcfdf1ece812c3b66b33faa
Danny Nielsen <danny.nlsn0@gmail.com>
2017-10-14 19:44:40 +0200

    Successfully forwarded message to correct queue.

--- XML-Translator-Component/src/com/group14/Main.java
+++ XML-Translator-Component/src/com/group14/Main.java
@@ -7,27 +7,22 @@ import java.util.concurrent.TimeoutException;
 
 public class Main {
 
-    private static final String HOST_NAME = "207.154.228.245";
     private static final String CONSUME_QUEUE_NAME = "Xml_Bank_Translator_Queue";
-    private static final String BANK_HOST = "10.18.144.10";
+    private static final String BANK_HOST = "datdb.cphbusiness.dk";
     private static final String PUBLISH_EXCHANGE_NAME = "cphbusiness.bankXML";
-    private static final String REPLY_QUEUE_NAME = "Bank_Response_Queue";
+    private static final String REPLY_QUEUE_NAME = "Group14_Bank_Response_Queue";
 
     public static void main(String[] args) throws Exception {
 
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost(BANK_HOST);
         Connection bankConnection = factory.newConnection();
-        factory.setHost(HOST_NAME);
-        Connection replyQueueConnection = factory.newConnection();
-        Channel channel = replyQueueConnection.createChannel();
 
-        Channel bankPublishChannel = bankConnection.createChannel(); //Channel that sends our request to the bank.
-        Channel bankConsumeChannel = bankConnection.createChannel(); //Channel that gets the response from the bank.
+        Channel bankPublishChannel = bankConnection.createChannel();
 
         //String xmlRequest = receiveMessage(channel);
         String xmlRequest = "<LoanRequest>    <ssn>12345678</ssn>    <creditScore>685</creditScore>    <loanAmount>1000.0</loanAmount>    <loanDuration>1973-01-01 01:00:00.0 CET</loanDuration> </LoanRequest>";
-        getBankXmlResponseAndForward(xmlRequest, bankPublishChannel, bankConsumeChannel);
+        getBankXmlResponseAndForward(xmlRequest, bankPublishChannel);
 
 
     }
@@ -54,35 +49,36 @@ public class Main {
 
     }
 
-    private static String getBankXmlResponseAndForward(String xmlRequest, Channel publishChannel, Channel consumeChannel) throws Exception{
-        publishChannel.exchangeDeclare(PUBLISH_EXCHANGE_NAME, "fanout");
 
-        String replyQueueName = REPLY_QUEUE_NAME;
+    private static void getBankXmlResponseAndForward(String xmlRequest, Channel channel) throws Exception{
+        channel.queueDeclare(REPLY_QUEUE_NAME, false, false, false, null);
+
         String replyKey = "xml";
 
-        System.out.println("Waiting for response from bank...");
+        System.out.println("Waiting for response...");
 
         //Sending request and routing the request using a builder.
         AMQP.BasicProperties builder = new AMQP.BasicProperties
                 .Builder()
-                .contentType("text/plain")
+                .contentType("application/xml")
                 .deliveryMode(1)
-                .replyTo(replyQueueName)
+                .replyTo(REPLY_QUEUE_NAME)
                 .build();
 
         //Publish and route message
-        consumeChannel.basicPublish(PUBLISH_EXCHANGE_NAME, replyKey, builder, xmlRequest.getBytes());
+        channel.basicPublish(PUBLISH_EXCHANGE_NAME, replyKey, builder, xmlRequest.getBytes());
 
-        //QueueingConsumer consumer = new QueueingConsumer(consumeChannel);
+        QueueingConsumer consumer = new QueueingConsumer(channel);
 
-        //consumeChannel.basicConsume(replyQueueName, false, consumer);
+        channel.basicConsume(REPLY_QUEUE_NAME, false, consumer);
 
-//        QueueingConsumer.Delivery delivery = consumer.nextDelivery();
-//        String response = new String(delivery.getBody());
-//
-//        System.out.println("[x] Forwarded following response from bank:");
-//        return response;
+        QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+        String response = new String(delivery.getBody());
 
-        return "[x] Forwarded bank response!";
+        channel.close();
+        channel.getConnection().close();
+        System.out.println("[x] forwarded response successfully:");
+        System.out.println(response);
+
     }
 }
