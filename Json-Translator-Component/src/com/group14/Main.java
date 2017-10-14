6fd824e82ba0756a18ee73c7b75006730af6e36d
Danny Nielsen <danny.nlsn0@gmail.com>
2017-10-14 20:20:19 +0200

    Added initial JSON translator project.

---
+++ Json-Translator-Component/src/com/group14/Main.java
@@ -0,0 +1,14 @@
+package com.group14;
+
+public class Main {
+
+    private static final String HOST_NAME = "207.154.228.245";
+    private static final String CONSUME_QUEUE_NAME = "Json_Bank_Translator_Queue";
+    private static final String BANK_HOST = "datdb.cphbusiness.dk";
+    private static final String PUBLISH_EXCHANGE_NAME = "cphbusiness.bankJSON";
+    private static final String REPLY_QUEUE_NAME = "Group14_Bank_Response_Queue";
+
+    public static void main(String[] args) {
+	  
+    }
+}
